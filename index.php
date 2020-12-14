00cd8d451e0862508cbd2508d3f84835b1ec6ef1
Student User <student@workstation.lab.example.com>
2020-12-14 10:39:05 -0500

    Change is comming

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
    <title>PHP Test</title>
     </head>
      <body>
-      <?php echo '<p>Hello  World php version is " . PHP_VERSION .</p>'; ?> 
+      <?php echo '<p>Hello  World php version is " . PHP_VERSION .</p>';
+        print "A Change is comming \n";
+       ?> 
+      
        </body>
        </html>
