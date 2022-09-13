e5e418e0312298bdeab48653f027d6f10a4710c2
icycofe <68822217+icycofe@users.noreply.github.com>
2022-09-13 15:48:24 +0800

    again

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
    <title>PHP Test</title>
     </head>
      <body>
-      <?php echo '<p>Hello  World php version is " . $PHP_VERSION .</p>'; ?> 
+      <?php echo "<p>Hello  World php version is " . phpversion() .'</p>'; ?> 
        </body>
        </html>
